3be5fe633bed811a0dbb5e741458417f6d7993c2
Everest <listiantoid@gmail.com>
2022-05-01 02:05:35 +0700

    First Commit

---
+++ app/src/main/java/com/everest/tokomu/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.everest.tokomu
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
